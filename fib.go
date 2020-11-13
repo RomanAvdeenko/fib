21da605cbd424ea9c8fc7dbfc3ab19bc7ccbf807
RomanAvdeenko <yes@hl.ua>
2020-11-14 00:16:50 +0200

    Make tests

--- fib.go
+++ fib.go
@@ -1,5 +1,8 @@
 package fib
 
-func fib(n int) int {
-	return 0
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
 }
--- fib_test.go
+++ fib_test.go
@@ -2,6 +2,9 @@ package fib_test
 
 import (
 	"testing"
+
+	"github.com/RomanAvdeenko/fib"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFib(t *testing.T) {
@@ -19,11 +22,39 @@ func TestFib(t *testing.T) {
 			in:   1,
 			want: 1,
 		},
+		{
+			name: "two",
+			in:   2,
+			want: 1,
+		},
+		{
+			name: "three",
+			in:   3,
+			want: 2,
+		},
+		{
+			name: "four",
+			in:   4,
+			want: 3,
+		}, {
+			name: "five",
+			in:   5,
+			want: 5,
+		},
+		{
+			name: "six",
+			in:   6,
+			want: 8,
+		}, {
+			name: "nine",
+			in:   9,
+			want: 34,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			assert.Equal()
+			assert.Equal(t, tc.want, fib.Fib(tc.in))
 		})
 	}
 
