eed7b00649fc6be8b4c15a56de3a486afdc1942a
RomanAvdeenko <yes@hl.ua>
2020-11-14 15:42:12 +0200

    Add docs

--- fib.go
+++ fib.go
@@ -1,5 +1,9 @@
+//Package provides implementation
+
 package fib
 
+// Fib1(int)int
+// It's a canonical slow fibonacci implementation.
 func Fib1(n int) int {
 	if n < 2 {
 		return n
@@ -7,6 +11,8 @@ func Fib1(n int) int {
 	return Fib1(n-1) + Fib1(n-2)
 }
 
+// Fib(int)int
+// It's a  caching map fibonacci implementation.
 func Fib(n int) int {
 	fn := make(map[int]int, n+1)
 	for i := 0; i <= n; i++ {
